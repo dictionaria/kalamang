78739f6d8dd99fd4a104dcf6a13f49d4a7756c2b
Johannes Englisch <englisch@shh.mpg.de>
2021-09-24 13:47:21 +0200

    require pydictionaria version with updated cdstar urls

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         'cldfbench',
         'pyglottolog',
-        'pydictionaria>=2.0',
+        'pydictionaria>=2.1',
     ],
     extras_require={
         'test': [
